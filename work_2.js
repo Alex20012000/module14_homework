4c901beef609c57747f277a34537196d1b62a46f
Alex20012000 <133386461+Alex20012000@users.noreply.github.com>
2023-07-06 19:13:05 +0300

    works 14 mod

---
+++ work_2.js
@@ -0,0 +1,31 @@
+const jsonString = `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`;
+   
+   const data = JSON.parse(jsonString);
+   const obj = {
+     list: [
+       {
+         name: data.list[0].name,
+         age: data.list[0].age,
+         prof: data.list[0].prof,
+       },
+       {
+         name: data.list[1].name,
+         age: data.list[1].age,
+         prof: data.list[1].prof,
+       },
+     ],
+   };
+   console.log(data)
